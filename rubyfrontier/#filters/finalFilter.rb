3e19061ceb120aa95f79e9417acd6315dc69cbf1
Jörg Kantel <joerg@kantel.de>
2018-10-15 20:35:45 +0200

    Kramdown

--- rubyfrontier/#filters/finalFilter.rb
+++ rubyfrontier/#filters/finalFilter.rb
@@ -1,3 +1,13 @@
 def finalFilter(adrPageTable)
   # adrPageTable[:renderedtext] = process(adrPageTable[:renderedtext])
+  if adrPageTable[:kramdown]
+    adrPageTable[:renderedtext] = adrPageTable[:renderedtext].gsub("><![CDATA", ">%<![CDATA")
+    adrPageTable[:renderedtext] = adrPageTable[:renderedtext].gsub("]]>", "%]]>")
+  end
+  # Unnötige leere Paragraphen entfernen:
+  adrPageTable[:renderedtext] = adrPageTable[:renderedtext].gsub("<p></p>\n\n", "")
+  if adrPageTable[:xml]
+    adrPageTable[:renderedtext] = adrPageTable[:renderedtext].gsub("<p><item>", "<item>")
+    adrPageTable[:renderedtext] = adrPageTable[:renderedtext].gsub("</item></p>", "</item>")
+  end
 end
--- rubyfrontier/#filters/pageFilter.rb
+++ rubyfrontier/#filters/pageFilter.rb
@@ -1,3 +1,16 @@
 def pageFilter(adrPageTable)
   # adrPageTable[:bodytext] = process(adrPageTable[:bodytext])
- end
+  # kramdown
+  if adrPageTable[:kramdown]
+    adrPageTable[:bodytext] = Kramdown::Document.new(adrPageTable[:bodytext],
+    :auto_ids => false, :entity_output => :numeric).to_html.gsub("&quot;", '"')
+  # but kramdown also substitutes &lt;% for <% and %&gt; for %>, so if we have macros they've just been stripped
+    adrPageTable[:bodytext] = adrPageTable[:bodytext].gsub("&lt;%", "<%")
+    adrPageTable[:bodytext] = adrPageTable[:bodytext].gsub("%&gt;", "%>")
+  # and kramdown substitutes =&gt; for => so we have to substitute that back
+    adrPageTable[:bodytext] = adrPageTable[:bodytext].gsub("=&gt;", "=>")
+  # we also need macros for removing the "smart quotes"
+    adrPageTable[:bodytext] = adrPageTable[:bodytext].gsub("&#8220;", "\"")
+    adrPageTable[:bodytext] = adrPageTable[:bodytext].gsub("&#8221;", "\"")
+  end
+end
