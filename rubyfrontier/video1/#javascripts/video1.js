42c4bd4f70928c4d43d1d8cfd3a632d4ab120953
Jörg Kantel <joerg@kantel.de>
2019-10-22 20:36:12 +0200

    Update video1.js

--- rubyfrontier/video1/#javascripts/video1.js
+++ rubyfrontier/video1/#javascripts/video1.js
@@ -1,22 +1,22 @@
 let movie;
 
 function preload() {
-	movie = createVideo("assets/sbahn.mp4");
-	movie.hide();
+  movie = createVideo("assets/sbahn.mp4");
+  movie.hide();
 }
 
 function setup() {
-	canvas = createCanvas(480, 360);
-	canvas.parent("myCanvas");
+  canvas = createCanvas(480, 360);
+  canvas.parent("myCanvas");
 }
 
 function mousePressed() {
-	movie.loop();
+  movie.loop();
 }
 
 function draw() {
-	background(0);
-	let v = mouseX/width;
-	image(movie, 0, 0);
-	filter(THRESHOLD, v);
+  background(0);
+  let v = mouseX / width;
+  image(movie, 0, 0);
+  filter(THRESHOLD, v);
 }
