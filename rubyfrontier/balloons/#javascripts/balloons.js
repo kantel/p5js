0b250628505247af4ea366a7d40a5d0c4de38493
Jörg Kantel <joerg@kantel.de>
2022-06-21 20:54:25 +0200

    Hot Air Balloon

--- rubyfrontier/balloons/#javascripts/balloons.js
+++ rubyfrontier/balloons/#javascripts/balloons.js
@@ -1,27 +1,39 @@
-let balloon1;
-let x, y, dx;
+let balloon = [];
+let bg;
+let x = [];
+let y = [];
+let dx = [];
+
+numBalloons = 6;
 
 function preload() {
-  balloon1 = loadImage("images/balloon1.png");
+  for (i = 0; i < numBalloons; i++) {
+    balloon[i] = loadImage("images/balloon" + (i + 1) + ".png");
+  }
+  bg = loadImage("images/background.jpg");
 }
 
 function setup() {
-  let myCanvas = createCanvas(640, 320);
+  let myCanvas = createCanvas(940, 315);
   myCanvas.parent("myCanvas");
-  x = 20;
-  dx = 0.5;
-  y = random(20, 220);
+  
+  for (i = 0; i < numBalloons; i++) {
+    x[i] = random(width + 100, width + 500);
+    y[i] = random(20, 100);
+    dx[i] = random(-0.7, -0.2);
+  }
 }
 
 function draw() {
-  background(200);
-  image(balloon1, x, y);
-  x += dx;
-  if (x > width + 20) {reset()};
+  background(bg);
+  for (i = 0; i < numBalloons; i++) {
+    image(balloon[i], x[i], y[i]);
+    x[i] += dx[i];
+    if (x[i] <= -100) {
+      x[i] = random(width + 100, width + 500);
+      y[i] = random(20, 100);
+      dx[i]= random(-0.7, -0.2);
+    }
+  }
 }
 
-function reset() {
-  x = random(-50, -20);
-  y = random(20, 220);
-  dx = random(0.2, 0.7);
-}
