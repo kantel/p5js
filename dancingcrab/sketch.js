e9d170b44caee981f75b2727c46a5269d522d9b6
Jörg Kantel <joerg@kantel.de>
2023-05-09 16:49:57 +0200

    Dancing Crab P5.js init

---
+++ dancingcrab/sketch.js
@@ -0,0 +1,17 @@
+const windowWidth = 640;
+const windowHeight = 416;
+const fps = 60;
+let img;
+let bg;
+
+function preload() {
+  
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(49, 197, 244);    // Hellblau
+}
