4e263e756d979bbcbba00897f134629fa2594274
Jörg Kantel <joerg@kantel.de>
2023-05-30 21:07:24 +0200

    P5play RPG created

---
+++ p5playrpg/stage01/sketch.js
@@ -0,0 +1,23 @@
+// Ein Retro RPG Stage 1
+// Mit P5.play und planck.js
+
+// Konstanten
+const winwidth = 640;
+const winheight = 480;
+
+// Globale Variablen
+
+
+// Hauptprogramm
+function setup() {
+  myCanvas = createCanvas(winwidth, winheight);
+  myCanvas.parent("mysketch");
+ } // end setup()
+
+function draw() {
+  background(150, 230, 240);  // Himmelblau
+  
+} // end draw()
+
+// End Hauptprogramm
+
