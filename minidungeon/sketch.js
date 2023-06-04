ff43a047e04d16af120a2ea9df4f8367f3197bba
Jörg Kantel <joerg@kantel.de>
2023-06-04 20:49:09 +0200

    Noch mehr Assets

---
+++ minidungeon/sketch.js
@@ -0,0 +1,14 @@
+// Mini Dungeon Game Stage 1
+
+// Konstanten
+const winwidth = 440;
+const winheight = 440;
+
+function setup() {
+  myCanvas = createCanvas(winwidth, winheight);
+  myCanvas.parent("mysketch");
+}
+
+function draw() {
+  background(71, 45, 60);
+}
