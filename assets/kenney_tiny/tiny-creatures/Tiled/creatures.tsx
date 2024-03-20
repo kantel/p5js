e1be33f7806d58377edcb981daeb9fa8784f017c
Jörg Kantel <1219909+kantel@users.noreply.github.com>
2024-03-20 20:45:54 +0100

    Assets added

---
+++ assets/kenney_tiny/tiny-creatures/Tiled/creatures.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="creatures" tilewidth="16" tileheight="16" spacing="1" tilecount="180" columns="10">
+ <image source="../Tilemap/tilemap.png" width="170" height="306"/>
+</tileset>
